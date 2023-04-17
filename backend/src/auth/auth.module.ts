44cef9f8a89a293b13c524e5c5b0b7dc4523e620
Igor Topal <i@itopal.ru>
2023-04-17 13:46:36 +0300

    fix/jwt-env

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -7,13 +7,15 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JwtStrategy } from './jwt.strategy';
 import { LocalStrategy } from './local.strategy';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 @Module({
   imports: [
     UsersModule,
     PassportModule,
     JwtModule.register({
-      secret: 'jwtSecret',
+      secret: process.env.JWT_SECRET,
       signOptions: { expiresIn: '3600s' },
     }),
   ],
